3e3818d4c9c50a3fcc62ee7ecb2e69eb11f748f2
ibrahimcs51 <engribrahimkhanokz@gmail.com>
2025-07-20 01:47:31 +0500

    jamil kha

--- dev.js
+++ dev.js
@@ -8,3 +8,14 @@ for(let i = 0; i < 5; i++) {
     console.log("Hello World");
 }       
 console.log("This is a loop iteration: " + i);
+
+
+console.log("This is a loop iteration: " + i);
+console.log("This is a loop iteration: " + i);
+
+console.log("This is a loop iteration: " + i);
+console.log("This is a loop iteration: " + i);
+console.log("This is a loop iteration: " + i); 
+
+const fs = require('fs');
+const path = require('path');
